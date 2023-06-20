c65f6cd94236529d1b7302a35d4114ce92d1675e
George Barrett <georgeaustinbarrett@gmail.com>
2023-06-20 20:26:50 +0100

    deposit method defined in account.js. this method updates the balance by adding the deposited amount and records the transaction details. the transaction and deposit details are added to the this.transactions array. test written for a scenario where a user deposits 5000. test running GREEN

--- js/account.js
+++ js/account.js
@@ -4,10 +4,17 @@ class Account {
 
     constructor() {
         this.transactions = [];
+        this.balance = 0;
     }
 
     deposit(amount, date) {
-        
+        const updatedBalance = this.balance += amount;
+        this.transactions.push({
+            amount: amount,
+            date: date,
+            type: 'deposit',
+            balance: updatedBalance
+        });
     }
 
     withdraw(amount, date) {
--- js/account.test.js
+++ js/account.test.js
@@ -6,5 +6,14 @@ describe('Account', () => {
         account = new Account();
 
         expect(account.transactions).toEqual([]);
-    })
-})
+    });
+
+    test('if 5000 is deposited then it should add to the account', () => {
+        account = new Account();
+        account.deposit(5000, '01/01/2023')
+        expect(account.transactions).toEqual([
+            { amount: 5000, date: '01/01/2023', type: 'deposit', balance: 5000 },
+        ]);
+    });
+
+});
