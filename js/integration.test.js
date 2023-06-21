749653c52cfd63702d797e556fca9e2126cf28f3
George Barrett <georgeaustinbarrett@gmail.com>
2023-06-21 08:45:24 +0100

    integration test added to ensure that a withdrawal transaction is displayed correctly on the console. test running GREEN

--- js/integration.test.js
+++ js/integration.test.js
@@ -5,8 +5,8 @@ describe('Integration of classes', () => {
 
     it('should add a deposit transaction and be displayed correctly on the console', () => {
         account = new Account();
-        account.deposit(1000, '01/01/2023')
-        const consoleSpy = jest.spyOn(console, 'log')
+        account.deposit(1000, '01/01/2023');
+        const consoleSpy = jest.spyOn(console, 'log');
         account.printStatement();
 
         expect(consoleSpy).toHaveBeenCalledWith(
@@ -14,6 +14,21 @@ describe('Integration of classes', () => {
         );
         expect(consoleSpy).toHaveBeenCalledWith(
             '01/01/2023 || 1000.00 || || 1000.00'
+        );
+    });
+
+    it('should add a withdrawal transaction and be displayed correctly on the console', () => {
+        account = new Account();
+        account.withdraw(500, '02/01/2023');
+        account.printStatement();
+
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            'date || credit || debit || balance'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '02/01/2023 || || 500.00 || -500.00'
         );
     });
 })
