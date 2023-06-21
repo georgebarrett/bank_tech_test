6b515d4fc8f75f4b6b7f682e453c1412d84c2497
George Barrett <georgeaustinbarrett@gmail.com>
2023-06-21 09:32:03 +0100

    integration test written to ensure that multiple transactions are displayed correctly on the console. test running GREEN

--- js/integration.test.js
+++ js/integration.test.js
@@ -31,4 +31,40 @@ describe('Integration of classes', () => {
             '02/01/2023 || || 500.00 || -500.00'
         );
     });
-})
+
+    it('should display multiple transactions correctly on the console', () => {
+        account = new Account();
+        account.deposit(1000, '01/01/2023');
+        account.deposit(1000, '02/01/2023');
+        account.withdraw(500, '03/01/2023');
+        account.deposit(2000, '04/01/2023');
+        account.withdraw(2000, '05/01/2023');
+        account.withdraw(1000, '06/01/2023');
+        account.printStatement();
+
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            'date || credit || debit || balance'
+        );
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '01/01/2023 || 1000.00 || || 1000.00'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '02/01/2023 || 1000.00 || || 2000.00'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '03/01/2023 || || 500.00 || 1500.00'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '04/01/2023 || 2000.00 || || 3500.00'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '05/01/2023 || || 2000.00 || 1500.00'
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+            '06/01/2023 || || 1000.00 || 500.00'
+        ); 
+    });
+});
